8bdc50e50cdf418fa2c5c7e53e6f2f763140fb9c
CHADAD <mohamed.chadad@nimbleways.com>
2021-02-09 12:42:06 +0100

    add service to the controller & create the 1st get

--- src/volreservation.API/Controllers/VolController.cs
+++ src/volreservation.API/Controllers/VolController.cs
@@ -1,9 +1,26 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using volreservation.API.Domain.Models;
+using volreservation.API.Domain.Services;
 
 namespace volreservation.API
 {
     [Route("/api/[controller]")]
     public class CategoriesController : Controller
     {
+        private readonly IVolService _volService;
+
+        public ValuesController(IVolService volService) {
+            _volService = volService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Vol>> GetAllAsync()
+        {
+            var vols = await _volService.ListAsync();
+            return vols;
+        }
+    }
     }
 }
