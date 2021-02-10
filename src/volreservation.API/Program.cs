dbc2731e8324a5c8bebd90ca91c1b0261ac5b6e9
CMohamed <chadad.mohamed@gmail.com>
2021-02-10 01:09:36 +0100

    go

--- src/volreservation.API/Program.cs
+++ src/volreservation.API/Program.cs
@@ -13,14 +13,29 @@ namespace volreservation.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // CreateHostBuilder(args).Build().Run();
+            var host = BuildWebHost(args);
+
+            using(var scope = host.Services.CreateScope())
+            using(var context = scope.ServiceProvider.GetService<AppDbContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+
+            host.Run();
         }
 
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();
+
+        /*
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                });*/
     }
 }
